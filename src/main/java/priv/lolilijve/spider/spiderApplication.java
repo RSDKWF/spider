ef5c7417374e4b33e6b529850736f39d3a8d8bd7
lolilijve <yj13456213632@gmail.com>
2018-06-15 15:21:24 +0800

    --yj

---
+++ src/main/java/priv/lolilijve/spider/spiderApplication.java
@@ -0,0 +1,21 @@
+package priv.lolilijve.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * ------------------------------
+ *
+ * @author : lolilijve
+ * @Email :  1042703214@qq.com
+ * @create : 2018-06-15
+ */
+@SpringBootApplication
+public class spiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(spiderApplication.class, args);
+    }
+
+}
